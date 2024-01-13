90b4fe2fa55bac31e920db03eec1e6e5c03b20e8
ad1107 <nguyenanhducxmen@gmail.com>
2024-01-13 22:58:11 +0700

    Comments added for clarity.

--- testing-graph.py
+++ testing-graph.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 
 # animation function
-def animate(i, data_lst, start_time, x_labels_str):
+def animate(data_lst, start_time, x_labels_str):
     # Adjust Step Size
     step_size = 5
 
@@ -15,7 +15,9 @@ def animate(i, data_lst, start_time, x_labels_str):
     end = 130
     flt = random.uniform(start, end)
     data_lst.append(flt)
-    data_lst = data_lst[-100:]
+    data_lst = data_lst[
+        -100:
+    ]  # Limit to 100 elements to save memory space, usually the graph does not actually contain that many.
 
     plt.gcf().set_facecolor("black")  # Black window
     graph.set_facecolor("black")  # Black background
@@ -24,9 +26,9 @@ def animate(i, data_lst, start_time, x_labels_str):
     graph.clear()
     graph.plot(
         data_lst, marker=".", linestyle="-", markersize=17, color="red"
-    )  # Use dots as markers
+    )  # Use dots as markers, dash for line
 
-    # Formatting
+    # Formatting labels
     graph.set_title("Line Graph Testing", fontsize=30, color="white")
     graph.set_ylabel(
         f"Simulated Heartrate from {start} - {end}", fontsize=15, color="white"
@@ -44,25 +46,25 @@ def animate(i, data_lst, start_time, x_labels_str):
     elapsed_seconds = elapsed_time.total_seconds()
     elapsed_str = time.strftime("%M:%S", time.gmtime(elapsed_seconds))
 
-    # Set the x-axis labels with a specified step size
-
-    # x_labels_str = [elapsed_str] * len(data_lst)
+    # Append the "Time elapsed" value to x.
     x_labels_str.append(elapsed_str)
     x_labels_str = x_labels_str[-100:]
 
+    # Show the ticks based on the step size
     step_size = step_size if step_size > 0 else 1
     visible_indices = list(range(0, len(data_lst), step_size))
     graph.set_xticks(visible_indices)
-
-    plt.tick_params(axis="y", labelsize=13)
     graph.set_xticklabels(
         [x_labels_str[i] for i in visible_indices], rotation=45, ha="right", fontsize=13
     )
 
+    # Increase font size for label y.
+    plt.tick_params(axis="y", labelsize=13)
+
     graph.annotate(
         f"Elapsed Time: {elapsed_str}",
-        (0.85, 0.95),
-        xycoords="axes fraction",
+        (0.85, 0.95),  # Coordinations
+        xycoords="axes fraction",  # Fraction of axes from lower left
         ha="right",
         va="center",
         fontsize=15,
@@ -75,9 +77,10 @@ plt.rcParams["figure.figsize"] = [1366 / 100, 768 / 100]
 plt.rcParams["figure.dpi"] = 75
 
 # Create empty list to store data
-# Create figure and axes objects
 data_lst = []
 x_labels_str = []
+
+# Create figure and axes objects
 fig, graph = plt.subplots()
 
 # Set the start time using the system clock
